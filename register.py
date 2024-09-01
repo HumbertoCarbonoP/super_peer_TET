70c2dadad76f87cfcdb3460e19d57b196d574966
hcarbono <hacarbonop@eafit.edu.co>
2024-09-01 13:56:46 -0500

    feat: se agrega búsqueda descentralizada

--- register.py
+++ register.py
@@ -1,43 +1,29 @@
-from flask import Flask, request, jsonify, after_this_request
+from flask import Flask, request, jsonify
 import requests
-import time
-import threading
 
 app = Flask(__name__)
 
-peers = []
+peers_files = {}
 
 def update_peers_neighbors(peer_address):
-    print(f"Updating neighbors for peer {peer_address}")
-    for peer in peers:
+    for peer in peers_files.keys():
         if peer != peer_address:
             try:
-                print("URL:", f"http://{peer}/update_neighbors")
-                response = requests.put(f"http://{peer}/update_neighbors", json={"registered_peers": peer_address})
+                response = requests.put(f"http://{peer}/update_neighbors", json={"registered_peers": peers_files})
                 response.raise_for_status()
             except requests.exceptions.RequestException as e:
                 print(f"Error updating peer {peer}: {e}")
-                
-# def update_peers_neighbors_async(peer_address):
-#     thread = threading.Thread(target=update_peers_neighbors, args=(peer_address,))
-#     thread.daemon = True
-#     thread.start()
 
 @app.route('/register_peer', methods=['POST'])
 def register_peer():
     data = request.get_json()
     peer_address = data.get('peer_address')
-    if peer_address not in peers:
-        peers.append(peer_address)
-        returning_peers = ""
-        for peer in peers:
-            if peer != peer_address:
-                returning_peers += f"{peer},"
-
+    peer_files = data.get('peer_files')
+    if peer_address not in peers_files.keys():
+        peers_files[peer_address] = peer_files
         update_peers_neighbors(peer_address)
-
         return jsonify({"message": f"Peer {peer_address} registered successfully",
-                        "registered_peers": f"{returning_peers[0:-1]}"}), 200
+                        "registered_peers": peers_files}), 200
     else:
         return jsonify({"message": f"Peer {peer_address} already registered"}), 400
 
